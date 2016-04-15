18d52ae3ae65e28d14d82e0d607a5d08fa684bb2
Matt Corallo <git@bluematt.me>
2016-04-15 12:20:02 -0700

    Add FEC-coding API

--- src/logging.cpp
+++ src/logging.cpp
@@ -151,6 +151,7 @@ const CLogCategoryDesc LogCategories[] =
     {BCLog::QT, "qt"},
     {BCLog::LEVELDB, "leveldb"},
     {BCLog::UDPNET, "udpnet"},
+    {BCLog::FEC, "fec"},
     {BCLog::ALL, "1"},
     {BCLog::ALL, "all"},
 };
