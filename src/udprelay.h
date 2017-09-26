cc2d69808f0b5ec15a28236d477b418ec0c511b3
Matt Corallo <git@bluematt.me>
2017-09-26 11:42:01 -0400

    Add support for background block/tx backfill in local device write

--- src/udprelay.h
+++ src/udprelay.h
@@ -7,6 +7,9 @@
 
 #include <udpnet.h>
 
+class CBlock;
+class CTransaction;
+
 void UDPRelayBlock(const CBlock& block);
 
 void BlockRecvInit();
@@ -16,5 +19,9 @@ void BlockRecvShutdown();
 bool HandleBlockTxMessage(UDPMessage& msg, size_t length, const CService& node, UDPConnectionState& state, const std::chrono::steady_clock::time_point& packet_process_start);
 
 void ProcessDownloadTimerEvents();
+
+// Each UDPMessage must be of sizeof(UDPMessageHeader) + MAX_UDP_MESSAGE_LENGTH in length!
+void UDPFillMessagesFromBlock(const CBlock& block, std::vector<UDPMessage>& msgs);
+void UDPFillMessagesFromTx(const CTransaction& tx, std::vector<UDPMessage>& msgs);
 
 #endif
