malandro0/bitcoin

If an unconnectable block is received from the local receive group, save it for later

src/outoforder.cpp
@@ -1,0 +1,144 @@
+// Copyright (c) 2018 Blockstream
+
+#include <chain.h>
+#include <chainparams.h>
+#include <consensus/validation.h>
+#include <dbwrapper.h>
+#include <primitives/block.h>
+#include <sync.h>
+#include <uint256.h>
+#include <util.h>
+#include <validation.h>
+
+#include <deque>
+#include <map>
+#include <memory>
+#include <utility>
+#include <vector>
+
+static int ExtractHeightFromBlock(const Consensus::Params& consensusParams, const std::shared_ptr<const CBlock> pblock)
+{
+    if (pblock->nBits > consensusParams.BIP34AssumedBits) {
+        // We can't be sure BIP34 is active, and without that we can't figure out the height yet
+        return -1;
+    }
+
+    if (pblock->vtx.size() < 1) return -1;
+    if (pblock->vtx[0]->vin.size() < 1) return -1;
+
+    const auto& scriptSig = pblock->vtx[0]->vin[0].scriptSig;
+    auto pc = scriptSig.begin();
+    opcodetype opcode;
+    std::vector<unsigned char> data;
+    if (!scriptSig.GetOp(pc, opcode, data)) return -1;
+    try {
+        CScriptNum bn(data, /*require minimal encoding=*/true);
+        return bn.getint();
+    }
+    catch (scriptnum_error) {
+        return -1;
+    }
+}
+
+static const char DB_SUBSEQUENT_BLOCK = 'S';
+
+static CCriticalSection cs_ooob;
+
+static CDBWrapper* GetOoOBlockDB() EXCLUSIVE_LOCKS_REQUIRED(cs_ooob)
+{
+    static CDBWrapper *ooob_db = nullptr;
+    if (!ooob_db) ooob_db = new CDBWrapper(GetDataDir() / "future_blocks", /*cache size=*/1024);
+    return ooob_db;
+}
+
+bool StoreOoOBlock(const CChainParams& chainparams, const std::shared_ptr<const CBlock> pblock) EXCLUSIVE_LOCKS_REQUIRED(cs_main)
+{
+    LOCK(cs_ooob);
+    CDBWrapper * const ooob_db = GetOoOBlockDB();
+    auto key = std::make_pair(DB_SUBSEQUENT_BLOCK, pblock->hashPrevBlock);
+    std::map<uint256, CDiskBlockPos> successors;
+
+    ooob_db->Read(key, successors);
+    if (successors.count(pblock->GetHash())) {
+        // Already have it stored, so nothing to do
+        return true;
+    }
+
+    // Figure out the block's height from BIP34
+    const Consensus::Params& consensusParams = chainparams.GetConsensus();
+    const int height = ExtractHeightFromBlock(consensusParams, pblock);
+    if (height < consensusParams.BIP34Height) return false;  // nonsensical
+
+    // Don't save blocks too far in the future, to prevent a DoS on pruning
+    if (height > int(chainActive.Height() + MIN_BLOCKS_TO_KEEP)) return false;
+
+    LogPrintf("Adding block %s (height %u) to out-of-order disk cache\n", pblock->GetHash().GetHex(), height);
+    CDiskBlockPos diskpos = SaveBlockToDisk(*pblock, height, chainparams, nullptr);
+    successors.emplace(pblock->GetHash(), diskpos);
+    if (!ooob_db->Write(key, successors)) {
+        LogPrintf("ERROR adding block %s to out-of-order disk cache\n", pblock->GetHash().GetHex());
+        return false;
+    }
+    return true;
+}
+
+void ProcessSuccessorOoOBlocks(const CChainParams& chainparams, const uint256& prev_block_hash)
+{
+    CDBWrapper *ooob_db = nullptr;
+    std::deque<uint256> queue;
+    queue.push_back(prev_block_hash);
+    for ( ; !queue.empty(); queue.pop_front()) {
+        uint256 head = queue.front();
+        auto key = std::make_pair(DB_SUBSEQUENT_BLOCK, head);
+
+        LOCK(cs_ooob);
+        std::map<uint256, CDiskBlockPos> successors;
+        {
+            LOCK(cs_main);
+            if (!ooob_db) ooob_db = GetOoOBlockDB();
+
+            ooob_db->Read(key, successors);
+        }
+
+        if (successors.empty()) continue;
+
+        for (const auto& successor : successors) {
+            std::shared_ptr<CBlock> pblock = std::make_shared<CBlock>();
+            CBlock& block = *pblock;
+            if (!ReadBlockFromDisk(block, successor.second, chainparams.GetConsensus())) {
+                continue;
+            }
+            LogPrintf("Accepting deferred block %s from out-of-order disk cache\n", block.GetHash().GetHex());
+            ProcessNewBlock(chainparams, pblock, /*force processing=*/false, /*is new block?=*/nullptr, &successor.second, /*do_ooob=*/false);
+            queue.push_back(pblock->GetHash());
+        }
+
+        ooob_db->Erase(key);
+    }
+}
+
+void CheckForOoOBlocks(const CChainParams& chainparams)
+{
+    std::vector<uint256> to_process;
+    {
+        LOCK(cs_ooob);
+        CDBWrapper * const ooob_db = GetOoOBlockDB();
+
+        std::unique_ptr<CDBIterator> pcursor(ooob_db->NewIterator());
+
+        LOCK(cs_main);
+        for (pcursor->Seek(std::make_pair(DB_SUBSEQUENT_BLOCK, uint256())); pcursor->Valid(); pcursor->Next()) {
+            std::pair<char, uint256> key;
+            if (!(pcursor->GetKey(key) && key.first == DB_SUBSEQUENT_BLOCK)) break;
+
+            const uint256& prev_block_hash = key.second;
+            if (mapBlockIndex.count(prev_block_hash)) {
+                to_process.push_back(prev_block_hash);
+            }
+        }
+    }
+
+    for (const auto& prev_block_hash : to_process) {
+        ProcessSuccessorOoOBlocks(chainparams, prev_block_hash);
+    }
+}

src/validation.cpp
@@ -3329,7 +3329,7 @@ bool ProcessNewBlockHeaders(const std::vector<CBlockHeader>& headers, CValidatio
 }
 
 /** Store block on disk. If dbp is non-nullptr, the file is known to already reside on disk */
-static CDiskBlockPos SaveBlockToDisk(const CBlock& block, int nHeight, const CChainParams& chainparams, const CDiskBlockPos* dbp) {
+CDiskBlockPos SaveBlockToDisk(const CBlock& block, int nHeight, const CChainParams& chainparams, const CDiskBlockPos* dbp) {
     unsigned int nBlockSize = ::GetSerializeSize(block, SER_DISK, CLIENT_VERSION);
     CDiskBlockPos blockPos;
     if (dbp != nullptr)
@@ -3431,7 +3431,7 @@ bool CChainState::AcceptBlock(const std::shared_ptr<const CBlock>& pblock, CVali
     return true;
 }
 
-bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<const CBlock> pblock, bool fForceProcessing, bool *fNewBlock)
+bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<const CBlock> pblock, bool fForceProcessing, bool *fNewBlock, const CDiskBlockPos *diskpos, const bool do_ooob)
 {
     AssertLockNotHeld(cs_main);
 
@@ -3447,7 +3447,7 @@ bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<cons
 
         if (ret) {
             // Store to disk
-            ret = g_chainstate.AcceptBlock(pblock, state, chainparams, &pindex, fForceProcessing, nullptr, fNewBlock);
+            ret = g_chainstate.AcceptBlock(pblock, state, chainparams, &pindex, fForceProcessing, diskpos, fNewBlock);
         }
         if (!ret) {
             GetMainSignals().BlockChecked(*pblock, state);
@@ -3460,6 +3460,11 @@ bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<cons
     CValidationState state; // Only used to report errors, not invalidity - ignore it
     if (!g_chainstate.ActivateBestChain(state, chainparams, pblock))
         return error("%s: ActivateBestChain failed", __func__);
+
+    if (do_ooob) {
+        // Check if we have any other blocks to process waiting on this one
+        ProcessSuccessorOoOBlocks(chainparams, pblock->GetHash());
+    }
 
     return true;
 }

ehsanen/bitcoinsatellite

src/outoforder.cpp
@@ -1,0 +1,144 @@
+// Copyright (c) 2018 Blockstream
+
+#include <chain.h>
+#include <chainparams.h>
+#include <consensus/validation.h>
+#include <dbwrapper.h>
+#include <logging.h>
+#include <primitives/block.h>
+#include <sync.h>
+#include <uint256.h>
+#include <validation.h>
+
+#include <deque>
+#include <map>
+#include <memory>
+#include <utility>
+#include <vector>
+
+static int ExtractHeightFromBlock(const Consensus::Params& consensusParams, const std::shared_ptr<const CBlock> pblock)
+{
+    if (pblock->nBits > consensusParams.BIP34AssumedBits) {
+        // We can't be sure BIP34 is active, and without that we can't figure out the height yet
+        return -1;
+    }
+
+    if (pblock->vtx.size() < 1) return -1;
+    if (pblock->vtx[0]->vin.size() < 1) return -1;
+
+    const auto& scriptSig = pblock->vtx[0]->vin[0].scriptSig;
+    auto pc = scriptSig.begin();
+    opcodetype opcode;
+    std::vector<unsigned char> data;
+    if (!scriptSig.GetOp(pc, opcode, data)) return -1;
+    try {
+        CScriptNum bn(data, /*require minimal encoding=*/true);
+        return bn.getint();
+    }
+    catch (scriptnum_error) {
+        return -1;
+    }
+}
+
+static const char DB_SUBSEQUENT_BLOCK = 'S';
+
+static CCriticalSection cs_ooob;
+
+static CDBWrapper* GetOoOBlockDB() EXCLUSIVE_LOCKS_REQUIRED(cs_ooob)
+{
+    static CDBWrapper *ooob_db = nullptr;
+    if (!ooob_db) ooob_db = new CDBWrapper(GetDataDir() / "future_blocks", /*cache size=*/1024);
+    return ooob_db;
+}
+
+bool StoreOoOBlock(const CChainParams& chainparams, const std::shared_ptr<const CBlock> pblock) EXCLUSIVE_LOCKS_REQUIRED(cs_main)
+{
+    LOCK(cs_ooob);
+    CDBWrapper * const ooob_db = GetOoOBlockDB();
+    auto key = std::make_pair(DB_SUBSEQUENT_BLOCK, pblock->hashPrevBlock);
+    std::map<uint256, FlatFilePos> successors;
+
+    ooob_db->Read(key, successors);
+    if (successors.count(pblock->GetHash())) {
+        // Already have it stored, so nothing to do
+        return true;
+    }
+
+    // Figure out the block's height from BIP34
+    const Consensus::Params& consensusParams = chainparams.GetConsensus();
+    const int height = ExtractHeightFromBlock(consensusParams, pblock);
+    if (height < consensusParams.BIP34Height) return false;  // nonsensical
+
+    // Don't save blocks too far in the future, to prevent a DoS on pruning
+    if (height > int(::ChainActive().Height() + MIN_BLOCKS_TO_KEEP)) return false;
+
+    LogPrintf("Adding block %s (height %u) to out-of-order disk cache\n", pblock->GetHash().GetHex(), height);
+    FlatFilePos diskpos = SaveBlockToDisk(*pblock, height, chainparams, nullptr);
+    successors.emplace(pblock->GetHash(), diskpos);
+    if (!ooob_db->Write(key, successors)) {
+        LogPrintf("ERROR adding block %s to out-of-order disk cache\n", pblock->GetHash().GetHex());
+        return false;
+    }
+    return true;
+}
+
+void ProcessSuccessorOoOBlocks(const CChainParams& chainparams, const uint256& prev_block_hash)
+{
+    CDBWrapper *ooob_db = nullptr;
+    std::deque<uint256> queue;
+    queue.push_back(prev_block_hash);
+    for ( ; !queue.empty(); queue.pop_front()) {
+        uint256 head = queue.front();
+        auto key = std::make_pair(DB_SUBSEQUENT_BLOCK, head);
+
+        LOCK(cs_ooob);
+        std::map<uint256, FlatFilePos> successors;
+        {
+            LOCK(cs_main);
+            if (!ooob_db) ooob_db = GetOoOBlockDB();
+
+            ooob_db->Read(key, successors);
+        }
+
+        if (successors.empty()) continue;
+
+        for (const auto& successor : successors) {
+            std::shared_ptr<CBlock> pblock = std::make_shared<CBlock>();
+            CBlock& block = *pblock;
+            if (!ReadBlockFromDisk(block, successor.second, chainparams.GetConsensus())) {
+                continue;
+            }
+            LogPrintf("Accepting deferred block %s from out-of-order disk cache\n", block.GetHash().GetHex());
+            ProcessNewBlock(chainparams, pblock, /*force processing=*/false, /*is new block?=*/nullptr, &successor.second, /*do_ooob=*/false);
+            queue.push_back(pblock->GetHash());
+        }
+
+        ooob_db->Erase(key);
+    }
+}
+
+void CheckForOoOBlocks(const CChainParams& chainparams)
+{
+    std::vector<uint256> to_process;
+    {
+        LOCK(cs_ooob);
+        CDBWrapper * const ooob_db = GetOoOBlockDB();
+
+        std::unique_ptr<CDBIterator> pcursor(ooob_db->NewIterator());
+
+        LOCK(cs_main);
+        for (pcursor->Seek(std::make_pair(DB_SUBSEQUENT_BLOCK, uint256())); pcursor->Valid(); pcursor->Next()) {
+            std::pair<char, uint256> key;
+            if (!(pcursor->GetKey(key) && key.first == DB_SUBSEQUENT_BLOCK)) break;
+
+            const uint256& prev_block_hash = key.second;
+            if (::BlockIndex().count(prev_block_hash)) {
+                to_process.push_back(prev_block_hash);
+            }
+        }
+    }
+
+    for (const auto& prev_block_hash : to_process) {
+        ProcessSuccessorOoOBlocks(chainparams, prev_block_hash);
+    }
+}

src/validation.cpp
@@ -3646,7 +3646,7 @@ bool ProcessNewBlockHeaders(const std::vector<CBlockHeader>& headers, CValidatio
 }
 
 /** Store block on disk. If dbp is non-nullptr, the file is known to already reside on disk */
-static FlatFilePos SaveBlockToDisk(const CBlock& block, int nHeight, const CChainParams& chainparams, const FlatFilePos* dbp) {
+FlatFilePos SaveBlockToDisk(const CBlock& block, int nHeight, const CChainParams& chainparams, const FlatFilePos* dbp) {
     unsigned int nBlockSize = ::GetSerializeSize(block, CLIENT_VERSION);
     FlatFilePos blockPos;
     if (dbp != nullptr)
@@ -3750,7 +3750,7 @@ bool CChainState::AcceptBlock(const std::shared_ptr<const CBlock>& pblock, CVali
     return true;
 }
 
-bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<const CBlock> pblock, bool fForceProcessing, bool *fNewBlock)
+bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<const CBlock> pblock, bool fForceProcessing, bool *fNewBlock, const FlatFilePos *diskpos, const bool do_ooob)
 {
     AssertLockNotHeld(cs_main);
 
@@ -3768,7 +3768,7 @@ bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<cons
         bool ret = CheckBlock(*pblock, state, chainparams.GetConsensus());
         if (ret) {
             // Store to disk
-            ret = ::ChainstateActive().AcceptBlock(pblock, state, chainparams, &pindex, fForceProcessing, nullptr, fNewBlock);
+            ret = ::ChainstateActive().AcceptBlock(pblock, state, chainparams, &pindex, fForceProcessing, diskpos, fNewBlock);
         }
         if (!ret) {
             GetMainSignals().BlockChecked(*pblock, state);
@@ -3781,6 +3781,11 @@ bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<cons
     CValidationState state; // Only used to report errors, not invalidity - ignore it
     if (!::ChainstateActive().ActivateBestChain(state, chainparams, pblock))
         return error("%s: ActivateBestChain failed (%s)", __func__, FormatStateMessage(state));
+
+    if (do_ooob) {
+        // Check if we have any other blocks to process waiting on this one
+        ProcessSuccessorOoOBlocks(chainparams, pblock->GetHash());
+    }
 
     return true;
 }